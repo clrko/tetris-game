261b15adda505b2d5b0ffe2fae2474071f6a6bcb
Claire Kodia <claire.kodia@gmail.com>
2020-09-14 18:36:15 +0200

    Styled tetris deck

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { createStage } from '../gameHelpers'
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris'
 /// Components
 import Stage from './Stage'
 import Display from './Display'
 import StartButton from './StartButton'
 
 const Tetris = () => (
-  <div>
-    <Stage stage={createStage()} />
-    <aside>
-      <div>
-        <Display text='Score' />
-        <Display text='Rows' />
-        <Display text='Level' />
-      </div>
-      <StartButton />
-    </aside>
-  </div>
+  <StyledTetrisWrapper>
+    <StyledTetris>
+      <Stage stage={createStage()} />
+      <aside>
+        <div>
+          <Display text='Score' />
+          <Display text='Rows' />
+          <Display text='Level' />
+        </div>
+        <StartButton />
+      </aside>
+    </StyledTetris>
+  </StyledTetrisWrapper>
 )
 
 export default Tetris
